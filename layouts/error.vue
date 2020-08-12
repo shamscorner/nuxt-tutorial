7a803580e1dc18fb56f8a503b407900489c4a91c
Shamim Hossain <hossains159@gmail.com>
2020-08-13 00:33:54 +0600

    custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1 class="text-xl text-red-500" v-if="error.statusCode === 404">
+      Page not found
+    </h1>
+    <h1 v-else>An error occurred</h1>
+    <div class="mx-4">
+      <NuxtLink to="/" class="text-blue-500">Home page</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'error',
+}
+</script>
