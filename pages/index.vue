8b1b61e34bc640639640e6550754870530759ef4
Shamim Hossain <hossains159@gmail.com>
2020-08-13 01:57:54 +0600

    process helper

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,9 @@
       <div class="my-2">
         <NuxtLink to="/about" class="button--grey">About Us</NuxtLink>
       </div>
+      <div>
+        <h1>I am rendered on the {{ renderedOn }} side</h1>
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +21,12 @@ export default {
     window.onNuxtReady(() => {
       console.log('Nuxt.js is ready and mounted')
     })
+  },
+
+  asyncData() {
+    return {
+      renderedOn: process.client ? 'client' : 'server',
+    }
   },
 }
 </script>
