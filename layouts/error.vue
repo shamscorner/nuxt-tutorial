94e0ec58abc6469f32345a5f8f14d5a23283ebb4
Shamim Hossain <hossains159@gmail.com>
2020-08-13 00:55:22 +0600

    healper function like

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container">
+  <div class="m-4">
     <h1 class="text-xl text-red-500" v-if="error.statusCode === 404">
       Page not found
     </h1>
     <h1 v-else>An error occurred</h1>
-    <div class="mx-4">
+    <div v-if="$nuxt.isOffline">You are offline</div>
+    <div v-else>You are online</div>
+    <div>
       <NuxtLink to="/" class="text-blue-500">Home page</NuxtLink>
     </div>
   </div>
