97b3dec361f0aee9a26818602f12f2c140e2d76a
Shamim Hossain <hossains159@gmail.com>
2020-08-14 00:20:43 +0600

    asyncData with

---
+++ pages/blog/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $http }) {
+    const post = await $http.$get(`https://api.nuxtjs.dev/posts/${params.id}`)
+    console.log('Data -> post', post)
+    return { post }
+  },
+}
+</script>
