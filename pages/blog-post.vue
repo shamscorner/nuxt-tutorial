b58833f01ff143513bf69dce94451ab5fcc31cc6
Shamim Hossain <hossains159@gmail.com>
2020-08-13 14:42:05 +0600

    fetch on server option

--- pages/blog-post.vue
+++ pages/blog-post.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    Welcome to the blog post
+    <h1 class="text-lg">Welcome to the blog post</h1>
+
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h3 class="text-lg text-blue-600 mt-4">{{ post.title }}</h3>
+        <p>{{ post.description }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'blog',
+
+  data() {
+    return {
+      posts: [],
+    }
+  },
+
+  async fetch() {
+    this.posts = await fetch('https://api.nuxtjs.dev/posts').then((res) =>
+      res.json()
+    )
+  },
+
+  fetchOnServer: false, // this will fetch the posts first and then will load the page -> client side
 }
 </script>
 
